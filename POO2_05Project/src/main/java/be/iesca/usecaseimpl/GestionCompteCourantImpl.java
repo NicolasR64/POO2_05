3c8d8bafe7d74755c0a636e11f143983da25f490
Ravaux Nicolas <la208319@student.helha.be>
2022-05-10 16:21:15 +0200

    Mise en commentaire en attendant le devellopement de dao

--- POO2_05Project/src/main/java/be/iesca/usecaseimpl/GestionCompteCourantImpl.java
+++ POO2_05Project/src/main/java/be/iesca/usecaseimpl/GestionCompteCourantImpl.java
@@ -5,57 +5,57 @@ import java.util.List;
 import be.iesca.dao.BiereDao;
 import be.iesca.daoimpl.DaoFactory;
 import be.iesca.domaine.Bundle;
-import be.iesca.domaine.Biere;
+import be.iesca.domaine.CompteCourant;
 import be.iesca.usecase.GestionCompteCourant;
 
 //TO DO
 public class GestionCompteCourantImpl implements GestionCompteCourant {
-	private BiereDao biereDao;
+	/*private ComtpeCourantDAO compteCourantDao;
 
 	public GestionCompteCourantImpl() {
-		this.biereDao = (BiereDao) DaoFactory.getInstance().getDaoImpl(BiereDao.class);
+		this.ComtpeCourantDAO = (compteCourantDao) DaoFactory.getInstance().getDaoImpl(CompteCourantDao.class);
 	}
 
 	@Override
 	public void lister(Bundle bundle) {
 		boolean listeOk = true;
 		String message = "";
-		List<Biere> listeBieres = null;
-		listeBieres = this.biereDao.listerBieres();
-		if (listeBieres==null) {
+		List<CompteCourant> listeCompteCourant = null;
+		listeCompteCourant = this.ComtpeCourantDAO.listerCompteCourant();
+		if (ComtpeCourantDAO==null) {
 			listeOk = false;
-		} else if (listeBieres.isEmpty())
+		} else if (ComtpeCourantDAO.isEmpty())
 			message = "Liste vide";
-		else if (listeBieres.size() == 1)
+		else if (ComtpeCourantDAO.size() == 1)
 			message = "Il y a 1 bière";
 		else
-			message = "Il y a " + listeBieres.size() + " bières";
+			message = "Il y a " + ComtpeCourantDAO.size() + " bières";
 		bundle.put(Bundle.OPERATION_REUSSIE, listeOk);
 		bundle.put(Bundle.MESSAGE, message);
-		bundle.put(Bundle.LISTE, listeBieres);
+		bundle.put(Bundle.LISTE, ComtpeCourantDAO);
 	}
 
 	@Override
-	public void modifierBiere(Bundle bundle) {
+	public void modifierComtpeCourantDAO(Bundle bundle) {
 		boolean modificationReussie = false;
 		String message = "";
-		Biere biere = (Biere) bundle.get(Bundle.BIERE);
-		if (biere.getNom() == null || biere.getNom().isEmpty()) {
-			message = "La modification n'a pas pu être réalisée. Il manque le nom de la bière";
-		} else if (biere.getBrasserie() == null
-				|| biere.getBrasserie().isEmpty()) {
+		ComtpeCourant comtpeCourant = (ComtpeCourant) bundle.get(Bundle.COMPTECOURANT);
+		if (comtpeCourant.getNom() == null || comtpeCourant.getNom().isEmpty()) {
+			message = "La modification n'a pas pu être réalisée. Il manque le nom du compte";
+		} else if (comtpeCourant.getBrasserie() == null
+				|| comtpeCourant.getBrasserie().isEmpty()) {
 			message = "La modification n'a pas pu être réalisée. Il manque le nom de la brasserie";
-		} else if (biere.getCouleur() == null || biere.getCouleur().isEmpty()) {
+		} else if (comtpeCourant.getCouleur() == null || comtpeCourant.getCouleur().isEmpty()) {
 			message = "La modification n'a pas pu être réalisée. Il manque la couleur";
-		} else if (biere.getType() == null || biere.getType().isEmpty()) {
+		} else if (comtpeCourant.getType() == null || comtpeCourant.getType().isEmpty()) {
 			message = "La modification n'a pas pu être réalisée. Il manque le type";
 
 		} else {
-			Biere biereDB = this.biereDao.getBiere(biere.getNom());
+			Biere biereDB = this.ComtpeCourantDAO.getBiere(comtpeCourant.getNom());
 			if (biereDB == null) {
 				message = "La modification n'a pas pu être réalisée. Cette bière n'existe pas";
 			} else {
-				modificationReussie = this.biereDao.modifierBiere(biere);
+				modificationReussie = this.ComtpeCourantDAO.modifierBiere(comtpeCourant);
 				if (modificationReussie) {
 					message = "Modification réalisée avec succès";
 				} else {
@@ -65,5 +65,5 @@ public class GestionCompteCourantImpl implements GestionCompteCourant {
 		}
 		bundle.put(Bundle.OPERATION_REUSSIE, modificationReussie);
 		bundle.put(Bundle.MESSAGE, message);
-	}
+	}*/
 }
