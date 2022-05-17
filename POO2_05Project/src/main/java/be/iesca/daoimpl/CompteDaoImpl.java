d84b8de6b33471e19978e775ddc8ecf90eaab4f8
Lorys <lorys.massaux@hotmail.com>
2022-05-17 15:49:51 +0200

    UPDATE FILE "CompteDaoImpl"
    Suppression des fonctions inutilisées

--- POO2_05Project/src/main/java/be/iesca/daoimpl/CompteDaoImpl.java
+++ POO2_05Project/src/main/java/be/iesca/daoimpl/CompteDaoImpl.java
@@ -11,8 +11,6 @@ import be.iesca.domaine.CompteCourant;
 
 public class CompteDaoImpl implements CompteDao {
 	private static final String GET = "SELECT * FROM comptes c WHERE c.numero = ?";
-	private static final String MAJ = "UPDATE comptes SET solde= ?, decouvert= ?, isCloture= ? where numero= ?";
-	private static final String LISTER = "SELECT * FROM comptes c ORDER BY c.numero";
 
 	// obligatoire pour pouvoir construire une instance avec newInstance()
 	public CompteDaoImpl() {
@@ -34,55 +32,6 @@ public class CompteDaoImpl implements CompteDao {
 				con.close();
 		} catch (Exception ex) {
 		}
-	}
-
-	@Override
-	public boolean modifierCompte(CompteCourant compte) {
-		boolean modificationReussie = false;
-		Connection con = null;
-		PreparedStatement ps = null;
-		try {
-			con = DaoFactory.getInstance().getConnexion();
-			ps = con.prepareStatement(MAJ);
-			String numero = compte.getNumero().trim();
-			ps.setString(4, numero); // <------------------- 4 ème paramètre !
-			ps.setDouble(1, compte.getSolde());
-			ps.setDouble(2, compte.getDecouvertMax());
-			ps.setBoolean(3, compte.isCloture());
-			int resultat = ps.executeUpdate();
-			if (resultat == 1) {
-				modificationReussie = true;
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-			cloturer(null, ps, con);
-		}
-		return modificationReussie;
-	}
-
-	@Override
-	public List<CompteCourant> listerCompte() {
-		List<CompteCourant> liste = new ArrayList<CompteCourant>();
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try {
-			con = DaoFactory.getInstance().getConnexion();
-			ps = con.prepareStatement(LISTER);
-			rs = ps.executeQuery();
-			String numero = "";
-			while (rs.next()) {
-				numero = rs.getString("numero");
-				CompteCourant compte = new CompteCourant(numero, rs.getDouble(2), rs.getDouble(3));
-				liste.add(compte);
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-			cloturer(rs, ps, con);
-		}
-		return liste;
 	}
 
 	@Override
